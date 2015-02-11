bce4c6c41c3a808e87fa636585b2515070b4eac5
project-yue <makingitbettergo@gmail.com>
2015-02-11 21:48:58 +1300

    commented underscore module

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var _ = require('underscore');
+// var _ = require('underscore');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
